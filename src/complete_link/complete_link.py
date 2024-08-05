f5195a7b267e4dd4490124d4d7ca69d4ad448af1
Rodinez <mngtandrade@gmail.com>
2024-08-05 17:16:55 -0300

    setup

---
+++ src/complete_link/complete_link.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('clusters', type=int)
+args = parser.parse_args()
+
+if args.clusters <= 0:
+    raise ValueError("The parameter 'clusters' must be a numeric value greater than 0")
+
+dataframe = pd.read_csv('../dados_e_planilha/datasets/c2ds1-2sp.txt', sep='\t', header=None, names=['sample_label', 'd1', 'd2'], skiprows=1)
+
+print(dataframe.head(1000))
