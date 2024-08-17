dec87d5dec59fe8a53ab227622f219a6070308d4
Rodinez <mngtandrade@gmail.com>
2024-08-17 18:09:27 -0300

    K_medias completed

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # IA_2024_1
+
+## Requirements
+Python 
+### Libraries
+pandas
+argparse
+spicy
+
+## How to run k_medias?
+python ./k_medias.py arg1 arg2 or py ./k_medias.py arg1 arg2
+
+- arg1 -> number of clusters
+- arg2 -> number of iterations
+
+the results will be at \out
--- src/k_medias/k_medias.py
+++ src/k_medias/k_medias.py
@@ -1,16 +1,96 @@
 import pandas as pd
 import argparse
+from scipy.spatial.distance import euclidean
 
 parser = argparse.ArgumentParser()
 parser.add_argument('clusters', type=int)
 parser.add_argument('iterations', type=int)
 args = parser.parse_args()
 
-if args.clusters <= 0:
-    raise ValueError("The parameter 'clusters' must be a numeric value greater than 0")
-if args.iterations <= 0:
-    raise ValueError("The parameter 'iterations' must be a numeric value greater than 0")
+args_dict = vars(args)
+clusters = args_dict['clusters']
+iterations = args_dict['iterations']
 
-dataframe = pd.read_csv('../dados_e_planilha/datasets/c2ds1-2sp.txt', sep='\t', header=None, names=['sample_label', 'd1', 'd2'], skiprows=1)
+try:
+    if clusters <= 0:
+        raise ValueError("The parameter 'clusters' must be a numeric value greater than 0")
+except ValueError as e:
+    print(e)
+    exit(1)
+try:
+    if iterations <= 0:
+        raise ValueError("The parameter 'iterations' must be a numeric value greater than 0")
+except ValueError as e:
+    print(e)
+    exit(1)
 
-print(dataframe.head(1000))
+dataframe = pd.read_csv('../dados_e_planilha/datasets/c2ds1-2sp.txt', sep='\t', header=None, names=['Sample', 'A1', 'A2'], skiprows=1)
+numSamples = dataframe.shape[0]
+
+try:
+    if clusters > numSamples:
+        raise ValueError(f"There are more clusters than samples, the number of clusters must be equal or lower than {numSamples}")
+except ValueError as e:
+    print(e)
+    exit(1)
+
+centroids = []
+numSamplesPerInitialCentroid = numSamples // clusters
+
+for i in range(clusters):
+    meanA1 = 0
+    meanA2 = 0
+    
+    for j in range(numSamplesPerInitialCentroid):
+        index = i * numSamplesPerInitialCentroid + j
+        meanA1 += dataframe.loc[index]['A1']
+        meanA2 += dataframe.loc[index]['A2']
+    
+    meanA1 /= numSamplesPerInitialCentroid
+    meanA2 /= numSamplesPerInitialCentroid
+    
+    centroidName = 'C'+ f'{i}'
+    centroids.append((centroidName, meanA1, meanA2))
+    
+euclideanDistances = [[] for _ in range(clusters)]
+lowerEuclideanDistances = []
+    
+for i in range(iterations):
+    for idx, centroid in enumerate(centroids):
+        valuesCentroid = (centroid[1], centroid[2])
+        for k in range(numSamples):
+            valuesSample = (dataframe.loc[k]['A1'], dataframe.loc[k]['A2'])
+            distance = euclidean(valuesCentroid, valuesSample)
+            euclideanDistances[idx].append(distance)
+    
+    for j in range(numSamples):
+        lowerDistancesToCentroids = min(range(clusters), key=lambda i: euclideanDistances[i][j])
+        lowerEuclideanDistances.append(lowerDistancesToCentroids)
+    
+    centroids.clear()
+    for j in range(clusters):
+        meanA1 = 0
+        meanA2 = 0
+        counter = lowerEuclideanDistances.count(j)
+        for k in range(numSamples):
+            if lowerEuclideanDistances[k] == j:
+                meanA1 += dataframe.loc[k]['A1']
+                meanA2 += dataframe.loc[k]['A2']
+        
+        meanA1 /= counter
+        meanA2 /= counter
+    
+        centroidName = 'C'+ f'{j}'
+        centroids.append((centroidName, meanA1, meanA2))
+    
+    if (i + 1) != iterations:
+        for j in range(clusters):
+            euclideanDistances[j].clear()
+        lowerEuclideanDistances.clear()
+
+resultDataframe = pd.DataFrame({
+    'sample_label': dataframe['Sample'],
+    'centroid_index': lowerEuclideanDistances
+})
+
+resultDataframe.to_csv("../out/k_medias_c2ds1-2sp.clu", sep=' ', index=False, header=False)              
