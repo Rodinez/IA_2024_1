18b6a34c2ed5def79df6cc284c515246c0034f3c
toyota-thiago <107651703+toyota-thiago@users.noreply.github.com>
2024-08-26 22:15:04 -0300

    rand.py now supports all three algorithms

--- src/Rand/rand.py
+++ src/Rand/rand.py
@@ -2,6 +2,7 @@ import pandas as pd
 import argparse
 import os
 import sklearn.metrics as sl
+import sys
 
 
 # essa função itera sobre as linhas do datagrama em bussca do ID passado
@@ -13,9 +14,11 @@ def nova_coluna(id, dataframeT):
 
 
 # essa função simula uma entrada no sistema, usa ela para o Google Colab
-datasetReal = 'monkeyReal1.clu'
+datasetReal = 'c2ds3-2gReal.clu'
+
 nivelDendrograma = '5'
-algoritmo = 'complete-link'
+
+algoritmo = 'kmedias'
 sys.argv = ['script.py', datasetReal, nivelDendrograma, algoritmo]
 
 parser = argparse.ArgumentParser()
@@ -49,44 +52,54 @@ dataframeR = pd.read_csv(
     header=None, names=['Sample', 'A1'], skiprows=0)
 
 #para montar o dataframe de teste, faremos de um jeito diferete:
-#como nele há os clusters e a lista de objetos pertencentes a cada cluster, transformaremos 
+#como nele há os clusters e a lista de objetos pertencentes a cada cluster, transformaremos
 #o cluster em uma coluna e o resto(objetos) em outra.
 data = []
 
 if (alg.startswith('k')):
-  alg = ''
-elif (alg.startswith('single')):
-  alg = '2' 
+  alg = '1'
+elif (alg.startswith('s')):
+  alg = '2'
 else:
   alg = '3'
 
-with open(f'out{alg}/{datasetT}', 'r') as file:
-    for line in file:
-        # Remover espaços extras e quebras de linha
-        line = line.strip()
-        
-        # Separar o primeiro elemento (sample) do restante
-        elementos = line.split(' ', 1)  # Divide no primeiro espaço encontrado
-        sample = elementos[0]
-        
-        # Verifica se há elementos adicionais
-        if len(elementos) > 1:
-            # Separa os elementos restantes por vírgula
-            valores = elementos[1].split(',')
-        else:
-            valores = []  # Lista vazia se não houver elementos
-        
-        # Adiciona os dados processados à lista
-        data.append({'Sample': sample, 'A1': valores})
+if (alg != '1'):
+  with open(f'out{alg}/{datasetT}', 'r') as file:
+      for line in file:
+          # Remover espaços extras e quebras de linha
+          line = line.strip()
 
-# Cria o DataFrame a partir da lista de dicionários
-dataframeT = pd.DataFrame(data)
+          # Separar o primeiro elemento (sample) do restante
+          elementos = line.split(' ', 1)  # Divide no primeiro espaço encontrado
+          sample = elementos[0]
 
+          # Verifica se há elementos adicionais
+          if len(elementos) > 1:
+              # Separa os elementos restantes por vírgula
+              valores = elementos[1].split(',')
+          else:
+              valores = []  # Lista vazia se não houver elementos
+
+          # Adiciona os dados processados à lista
+          data.append({'Sample': sample, 'A1': valores})
+
+  # Cria o DataFrame a partir da lista de dicionários
+  dataframeT = pd.DataFrame(data)
+else:
+  dataframeT = pd.read_csv(f'out/{datasetT}', sep=' ', header=None, names=['Sample', 'A1'], skiprows=0)
+  display(dataframeR)
+  display(dataframeT)
+  print(f"O índice Rand ajustado é  {sl.adjusted_rand_score(dataframeR['A1'],dataframeT['A1'])}")
+  sys.exit()
 #Para facilitar a busca por cluster, analisaremos apenas a parte numérica do identificador do objeto
 dataframeR['Sample'] = dataframeR['Sample'].str[offset:]
+if (offset == 11):
+  dataframeR.loc[1999:, 'Sample'] = dataframeR.loc[1999:, 'Sample'].str[1:]
 
 #Criamos uma nova coluna contendo o clusters em que o objeto foi classificado nos testes.
 
+display(dataframeR)
+display(dataframeT)
 dataframeR['B1'] = dataframeR['Sample'].apply(lambda id: nova_coluna(str((int(id)-1)), dataframeT))
 
 display(dataframeR)
